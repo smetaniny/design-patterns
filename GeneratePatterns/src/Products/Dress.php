a42d5e63c90f8f9e3c991fd08f1e75e9446b6792
Сергей <sm.sergey.v@yandex.ru>
2024-10-02 09:24:00 +0300

    Update Dress.php

--- GeneratePatterns/src/Products/Dress.php
+++ GeneratePatterns/src/Products/Dress.php
@@ -3,23 +3,41 @@ declare(strict_types=1);
 
 namespace App\GangOfFourDesignPatterns\GeneratePatterns\src\Products;
 
-
 use App\GangOfFourDesignPatterns\GeneratePatterns\src\Products\Contracts\ClothingItem;
 
+/**
+ * Класс Dress представляет платье и реализует интерфейс ClothingItem.
+ */
 class Dress implements ClothingItem {
-    private $description;
-    private $price;
+    private string $description;
+    private float $price;
 
-    public function __construct($description, $price) {
+    /**
+     * Конструктор класса Dress.
+     *
+     * @param string $description Описание платья.
+     * @param float $price Цена платья.
+     */
+    public function __construct(string $description, float $price) {
         $this->description = $description;
         $this->price = $price;
     }
 
+    /**
+     * Возвращает описание платья.
+     *
+     * @return string Описание платья.
+     */
     public function getDescription(): string
     {
         return $this->description;
     }
 
+    /**
+     * Возвращает цену платья.
+     *
+     * @return float Цена платья.
+     */
     public function getPrice(): float
     {
         return $this->price;
