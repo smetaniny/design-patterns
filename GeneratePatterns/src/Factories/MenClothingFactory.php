1fb69892bfda073ddf144d4597b14937a9c9ef91
Сергей <sm.sergey.v@yandex.ru>
2024-10-02 09:27:19 +0300

    Update MenClothingFactory.php

--- GeneratePatterns/src/Factories/MenClothingFactory.php
+++ GeneratePatterns/src/Factories/MenClothingFactory.php
@@ -6,13 +6,27 @@ namespace App\GangOfFourDesignPatterns\GeneratePatterns\src\Factories;
 use App\GangOfFourDesignPatterns\GeneratePatterns\src\Products\Contracts\ClothingItem;
 use App\GangOfFourDesignPatterns\GeneratePatterns\src\Products\Suit;
 
+/**
+ * Конкретная фабрика для создания мужской одежды.
+ * 
+ * Реализует метод создания предметов одежды на основе типа.
+ */
 class MenClothingFactory extends AbstractClothingFactory {
-    public function createClothingItem($type): ClothingItem
+    
+    /**
+     * Создает мужской предмет одежды на основе переданного типа.
+     *
+     * @param string $type Тип одежды (например, "костюм").
+     * @return ClothingItem Возвращает объект мужской одежды (например, костюм).
+     */
+    public function createClothingItem(string $type): ClothingItem
     {
+        // Проверяем тип создаваемого предмета
         if ($type === 'костюм') {
             return new Suit("Мужской костюм", 7000);
         }
 
+        // Если тип не распознан, по умолчанию возвращаем мужской костюм
         return new Suit("Мужской костюм", 7000);
     }
 }
