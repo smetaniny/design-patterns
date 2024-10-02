f996a24dbf781b6efa678c639ea07e463529ef53
Сергей <sm.sergey.v@yandex.ru>
2024-10-02 09:30:23 +0300

    Update WomenClothingFactory.php

--- GeneratePatterns/src/Factories/WomenClothingFactory.php
+++ GeneratePatterns/src/Factories/WomenClothingFactory.php
@@ -7,14 +7,30 @@ use App\GangOfFourDesignPatterns\GeneratePatterns\src\Products\Contracts\Clothin
 use App\GangOfFourDesignPatterns\GeneratePatterns\src\Products\Dress;
 use App\GangOfFourDesignPatterns\GeneratePatterns\src\Products\Suit;
 
+/**
+ * Фабрика для создания женской одежды.
+ * 
+ * Реализует абстрактный метод для создания различных типов женской одежды,
+ * таких как костюмы и платья.
+ */
 class WomenClothingFactory extends AbstractClothingFactory {
-    public function createClothingItem($type): ClothingItem
+    
+    /**
+     * Создает предмет женской одежды в зависимости от типа.
+     *
+     * @param string $type Тип создаваемого предмета одежды (например, костюм или платье).
+     * @return ClothingItem Возвращает объект типа ClothingItem (костюм или платье).
+     */
+    public function createClothingItem(string $type): ClothingItem
     {
+        // Проверяем тип создаваемого предмета одежды
         if ($type === 'костюм') {
             return new Suit("Женский костюм", 8000);
         } elseif ($type === 'платье') {
             return new Dress("Женское платье", 6000);
         }
-        // Можно добавить другие типы одежды
+
+        // Можно добавить обработку для других типов одежды, если они потребуются
+        throw new \InvalidArgumentException("Неизвестный тип одежды: $type");
     }
 }
