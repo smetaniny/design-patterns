696f54ee11276eaff751be661cda0dd9b6877a3f
Сергей <sm.sergey.v@yandex.ru>
2024-10-02 09:19:28 +0300

    Update ClothingPrototype.php

--- GeneratePatterns/src/Prototypes/ClothingPrototype.php
+++ GeneratePatterns/src/Prototypes/ClothingPrototype.php
@@ -2,6 +2,17 @@
 
 namespace App\GangOfFourDesignPatterns\GeneratePatterns\src\Prototypes;
 
+/**
+ * Абстрактный класс ClothingPrototype определяет интерфейс для клонирования объектов одежды.
+ * Этот класс служит базой для всех конкретных прототипов одежды (например, костюм, платье и т.д.).
+ */
 abstract class ClothingPrototype {
+
+    /**
+     * Абстрактный метод для клонирования объекта.
+     * Каждый подкласс должен реализовать этот метод, возвращающий клон объекта.
+     *
+     * @return mixed
+     */
     abstract public function clone();
 }
