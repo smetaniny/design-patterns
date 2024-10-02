8d554333cad49c2cdda1cddafdb791810ee89867
Сергей <sm.sergey.v@yandex.ru>
2024-10-02 09:18:09 +0300

    Update DressPrototype.php

--- GeneratePatterns/src/Prototypes/DressPrototype.php
+++ GeneratePatterns/src/Prototypes/DressPrototype.php
@@ -2,16 +2,35 @@
 
 namespace App\GangOfFourDesignPatterns\GeneratePatterns\src\Prototypes;
 
-
 use App\GangOfFourDesignPatterns\GeneratePatterns\src\Products\Dress;
 
+/**
+ * Класс DressPrototype реализует паттерн Прототип для объектов платья (Dress).
+ * Этот класс позволяет клонировать объект платья, не завися от его конкретного типа.
+ */
 class DressPrototype extends ClothingPrototype {
+    
+    /**
+     * Экземпляр платья, который будет использоваться в качестве прототипа.
+     *
+     * @var Dress
+     */
     private $dress;
 
+    /**
+     * Конструктор принимает объект платья, который будет клонироваться.
+     *
+     * @param Dress $dress Объект платья для клонирования.
+     */
     public function __construct(Dress $dress) {
         $this->dress = $dress;
     }
 
+    /**
+     * Клонирует объект платья.
+     *
+     * @return Dress Возвращает копию объекта платья.
+     */
     public function clone(): Dress {
         return clone $this->dress;
     }
