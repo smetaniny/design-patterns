50cae3bc252ebcf10fcc2ac40e953f47891ad26d
Сергей <sm.sergey.v@yandex.ru>
2024-10-02 09:29:21 +0300

    Update SuitFactory.php

--- GeneratePatterns/src/Factories/SuitFactory.php
+++ GeneratePatterns/src/Factories/SuitFactory.php
@@ -3,12 +3,20 @@ declare(strict_types=1);
 
 namespace App\GangOfFourDesignPatterns\GeneratePatterns\src\Factories;
 
-
 use App\GangOfFourDesignPatterns\GeneratePatterns\src\Products\Suit;
 
+/**
+ * Фабрика для создания объектов класса Suit (костюм).
+ */
 class SuitFactory {
+    
+    /**
+     * Создает объект костюма с предопределенными свойствами.
+     *
+     * @return Suit Возвращает новый объект костюма с описанием и ценой.
+     */
     public function createSuit(): Suit {
-        // Логика создания костюма
+        // Логика создания костюма с фиксированными параметрами
         return new Suit("Классический костюм", 300);
     }
 }
