a5a9e456d0cce57b0a9a574e200387c3b4ca8bc1
Сергей <sm.sergey.v@yandex.ru>
2024-10-02 09:26:13 +0300

    Update AbstractClothingFactory.php

--- GeneratePatterns/src/Factories/AbstractClothingFactory.php
+++ GeneratePatterns/src/Factories/AbstractClothingFactory.php
@@ -5,6 +5,17 @@ namespace App\GangOfFourDesignPatterns\GeneratePatterns\src\Factories;
 
 use App\GangOfFourDesignPatterns\GeneratePatterns\src\Products\Contracts\ClothingItem;
 
+/**
+ * Абстрактный класс AbstractClothingFactory, представляющий фабрику для создания предметов одежды.
+ * 
+ * Конкретные фабрики должны реализовывать метод createClothingItem для создания определенных типов одежды.
+ */
 abstract class AbstractClothingFactory {
+    /**
+     * Создает предмет одежды на основе переданного типа.
+     *
+     * @param string $type Тип создаваемого предмета одежды (например, "костюм" или "платье").
+     * @return ClothingItem Возвращает объект, реализующий интерфейс ClothingItem.
+     */
     abstract public function createClothingItem(string $type): ClothingItem;
 }
