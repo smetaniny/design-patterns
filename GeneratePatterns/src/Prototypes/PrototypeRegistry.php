3e7ababde0a97bcc22ff06d573355264e8d24d07
Сергей <sm.sergey.v@yandex.ru>
2024-10-02 09:17:37 +0300

    Update PrototypeRegistry.php

--- GeneratePatterns/src/Prototypes/PrototypeRegistry.php
+++ GeneratePatterns/src/Prototypes/PrototypeRegistry.php
@@ -3,13 +3,35 @@ declare(strict_types=1);
 
 namespace App\GangOfFourDesignPatterns\GeneratePatterns\src\Prototypes;
 
+/**
+ * Класс PrototypeRegistry управляет реестром прототипов одежды, предоставляя
+ * возможность регистрировать и получать клонируемые объекты.
+ */
 class PrototypeRegistry {
+    /**
+     * Ассоциативный массив для хранения зарегистрированных прототипов одежды.
+     *
+     * @var array
+     */
     private $prototypes = [];
 
-    public function register(string $name, ClothingPrototype $prototype) {
+    /**
+     * Регистрирует новый прототип в реестре.
+     *
+     * @param string $name Название или ключ прототипа.
+     * @param ClothingPrototype $prototype Объект прототипа, который должен быть зарегистрирован.
+     */
+    public function register(string $name, ClothingPrototype $prototype): void {
         $this->prototypes[$name] = $prototype;
     }
 
+    /**
+     * Получает прототип из реестра по его имени.
+     *
+     * @param string $name Имя зарегистрированного прототипа.
+     *
+     * @return ClothingPrototype|null Возвращает объект прототипа или null, если прототип с таким именем не найден.
+     */
     public function get(string $name): ?ClothingPrototype {
         return $this->prototypes[$name] ?? null;
     }
