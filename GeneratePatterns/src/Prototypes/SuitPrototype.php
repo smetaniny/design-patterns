97f6f2ac276a166de2fa03ce65c0739fb4c31b2a
Сергей <sm.sergey.v@yandex.ru>
2024-10-02 09:17:00 +0300

    Update SuitPrototype.php

--- GeneratePatterns/src/Prototypes/SuitPrototype.php
+++ GeneratePatterns/src/Prototypes/SuitPrototype.php
@@ -5,14 +5,33 @@ namespace App\GangOfFourDesignPatterns\GeneratePatterns\src\Prototypes;
 
 use App\GangOfFourDesignPatterns\GeneratePatterns\src\Products\Suit;
 
+/**
+ * Класс SuitPrototype реализует паттерн "Прототип" для создания копий объектов костюмов.
+ */
 class SuitPrototype extends ClothingPrototype {
+    /**
+     * Экземпляр костюма, который будет клонироваться.
+     *
+     * @var Suit
+     */
     private $suit;
 
+    /**
+     * Конструктор, принимающий объект костюма, который будет использован для клонирования.
+     *
+     * @param Suit $suit Исходный объект костюма.
+     */
     public function __construct(Suit $suit) {
         $this->suit = $suit;
     }
 
+    /**
+     * Метод для клонирования костюма.
+     *
+     * @return Suit Возвращает новый клон объекта костюма.
+     */
     public function clone(): Suit {
+        // Создаем клон костюма с использованием встроенного механизма клонирования PHP.
         return clone $this->suit;
     }
 }
